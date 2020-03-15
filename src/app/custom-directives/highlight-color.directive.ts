108ff699d61297a52512434484bb83814620b940
Nagalakshman Chirukuri <chirukurinagalakshman@outlook.com>
2020-03-15 11:52:32 +0000

    custom directive HostListner and hostBinding

--- src/app/custom-directives/highlight-color.directive.ts
+++ src/app/custom-directives/highlight-color.directive.ts
@@ -1,23 +1,27 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightColor]'
 })
 export class HighlightColorDirective implements OnInit {
 
+  @HostBinding('style.color') color: string = 'pink';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     // this.elementRef.nativeElement.style.color = 'red';
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'pink');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'pink');
   }
 
   @HostListener('mouseenter') mouseEnter() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
+    this.color = 'red';
   }
 
   @HostListener('mouseleave') mouseLevae() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'pink');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'pink');
+    this.color = 'pink';
   }
 
 }
